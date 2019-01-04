75123c6ba49e9fab7caf4571871b4c34e7f35684
frankievalentine <frankievalentine9@gmail.com>
2019-01-03 21:17:34 -0800

    Built protected route authorization component and context

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-const Account = () => (
-  <div>
-    <h1>Account</h1>
-  </div>
+import { AuthUserContext, withAuthorization } from "../Session";
+import { PasswordForgetForm } from "../PasswordForget";
+import PasswordChangeForm from "../PasswordChange";
+
+const AccountPage = () => (
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
-export default Account;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AccountPage);
--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AuthUserContext from './context'
 import withAuthentication from './withAuthentication'
+import withAuthorization from './withAuthorization'
 
 const Session = () => (
   <div>
@@ -10,4 +11,4 @@ const Session = () => (
 
 export default Session;
 
-export { AuthUserContext, withAuthentication };
+export { AuthUserContext, withAuthentication, withAuthorization };
